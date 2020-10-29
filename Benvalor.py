1648859139edbba1c65bb504e19b99df197d9504
alejandrofu <fu.alejandro@gmail.com>
2020-10-29 15:26:25 -0400

    Benjamin Graham Fromula del Valor intrinseco de la accion

---
+++ Benvalor.py
@@ -0,0 +1,9 @@
+# Benjamin Graham valor intrinseco de la accion
+
+EPS = float(input("Beneficio neto por accion: "))
+G = float(input("Crecimiento anual esperado del beneficio neto durante los pŕoxios 10 años: "))
+TNX = float(input("Trasury Yield 10 Years: "))
+
+# Formula
+V = EPS * (8.5+2*G)*TNX
+print(V)
