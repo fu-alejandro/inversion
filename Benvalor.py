909362628dd90b5433d2b63e7140fd8b64de415d
alejandrofu <fu.alejandro@gmail.com>
2020-10-30 13:22:27 -0400

    Correccion descripcion

--- Benvalor.py
+++ Benvalor.py
@@ -2,7 +2,7 @@
 
 EPS = float(input("Beneficio neto por accion: "))
 G = float(input("Crecimiento anual esperado del beneficio neto durante los pŕoxios 10 años: "))
-TNX = float(input("Trasury Yield 10 Years: "))
+TNX = float(input("Treasury Yield 10 Years: "))
 
 # Formula
 V = EPS * (8.5+2*G)*TNX
