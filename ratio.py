66e99e87f1c0e11d366df3d6e34361670f98160a
alejandrofu <fu.alejandro@gmail.com>
2020-10-30 18:38:02 -0400

    Add more ratio

--- ratio.py
+++ ratio.py
@@ -1,14 +1,14 @@
-# Ratio P/FCF
+# Ratio P/FCF Price to Free Cash Flow
 P = float(input("Market Capitalization: "))
 FCF = float(input("Free Cash Flow: "))
 R = P / FCF
 
-# Ratio PE Resultado son los años para recuperar el capital o inversion.
+# Ratio PE o PER Price Earning Ratio, Resultado son los años para recuperar el capital o inversion.
 P = float(input("Market Value per share: "))
 E = float(input("Earning per share: "))
 R = P / E
 
-# Ratio ROE Return on  Equity Resultado porcentaje anual para recuperar el capital o inversion.
-P = float(input("Market Value per share: "))
-E = float(input("Earning per share: "))
-R = P / E
+# Ratio ROE Return on Equity, Resultado porcentaje anual para recuperar el capital o inversion.
+E = float(input("Beficio: "))
+K = float(input("Capital: "))
+R = E / K
