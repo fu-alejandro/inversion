aa9481fe13704d36540c0aa102f8a706e9c8f182
alejandrofu <fu.alejandro@gmail.com>
2020-10-30 18:24:13 -0400

    Ratio mas utilizados en Inversion

---
+++ ratio.py
@@ -0,0 +1,14 @@
+# Ratio P/FCF
+P = float(input("Market Capitalization: "))
+FCF = float(input("Free Cash Flow: "))
+R = P / FCF
+
+# Ratio PE Resultado son los años para recuperar el capital o inversion.
+P = float(input("Market Value per share: "))
+E = float(input("Earning per share: "))
+R = P / E
+
+# Ratio ROE Return on  Equity Resultado porcentaje anual para recuperar el capital o inversion.
+P = float(input("Market Value per share: "))
+E = float(input("Earning per share: "))
+R = P / E
