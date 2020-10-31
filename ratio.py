8f27619a70ed2e2df33d9beeb5a0aace2bbaf77c
alejandrofu <fu.alejandro@gmail.com>
2020-10-30 20:35:02 -0400

    Add more ratio actualizacion print(R)

--- ratio.py
+++ ratio.py
@@ -2,23 +2,28 @@
 P = float(input("Market Capitalization: "))
 FCF = float(input("Free Cash Flow: "))
 R = P / FCF
+print(R)
 
 # Ratio PE o PER Price Earning Ratio, Resultado son los años para recuperar el capital o inversion.
 P = float(input("Market Value per share: "))
 E = float(input("Earning per share: "))
 R = P / E
+print(R)
 
 # Ratio ROE Return on Equity, Resultado porcentaje anual para recuperar el capital o inversion.
 E = float(input("Beficio: "))
 K = float(input("Capital: "))
 R = E / K
+print(R)
 
 # Ratio Debt / Equit,
 D = float(input("Total Liabilities: "))
 Q = float(input("Total share holders: "))
 R = D / Q
+print(R)
 
 # Ratio Sales
 P = float(input("Market Value per share: "))
 E = float(input("Earning per share: "))
 R = P / E
+print(R)
