0abd9e7b800a5fa2ead4a29db472fa9f69977c4f
alejandrofu <fu.alejandro@gmail.com>
2020-11-17 08:38:10 -0400

    Add formula GN tasa de crecimiento en N tiempo

--- Benvalor.py
+++ Benvalor.py
@@ -3,7 +3,18 @@
 EPS = float(input("Beneficio neto por accion: "))
 G = float(input("Crecimiento anual esperado del beneficio neto durante los pŕoxios 10 años: "))
 TNX = float(input("Treasury Yield 10 Years: "))
-
+MS = float(input("Margen de Seguridad: "))
 # Formula
-V = EPS * (8.5+2*G)*TNX
+V = EPS * (8.5+2*G)*TNX*MS
 print(V)
+
+# Calculo Multiplicador de beneficio anual en tasa de crecimiento previstas,
+# basadas en la formula simplificada de Benjamin Graham
+# tabla 11.4 , pagina 295, Libro Inversor Inteligente de Benjamin Graham
+
+G = float(input("Crecimiento anual esperado del beneficio neto durante los pŕoxios 10 años: "))
+M = float(input("Temporalidad de la tasa de crecimiento en Unidad Años: "))
+N = float(input("Temporalidad Final en Unidad Años: "))
+# fOrmula
+GN = ((1+G))^(M/N)-1)*100
+print(GN)
